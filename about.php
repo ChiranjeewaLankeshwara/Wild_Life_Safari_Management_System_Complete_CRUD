38abe6d3eefef65388737711c9d94be9939b3134
Chiranjeewa Peellawatta <chiranlankeshwara2003@gmail.com>
2024-11-25 23:58:49 +0530

    Update about.php

--- about.php
+++ about.php
@@ -15,3 +15,57 @@
     <link rel="stylesheet" href="./css/style.css">
 
 </head>
+
+<body>
+
+<!-- header section starts -->
+<section class="header">
+
+<a href="home.php" class="logo"><span>S</span>afari <span>L</span>anka</a>
+
+    <nav class="navbar">
+        <a href="about.php">About Us</a>
+        <a href="./contact_us/index.php">Contact Us</a>
+        <a href="package.php">Our Packages</a>
+        <a href="./reservations/add.php">Reservations</a>
+        <a href="./payment/add_payment.php">Check Out</a>
+        <a href="donation/addDonation.php">Donate Now!</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+<div class="heading" style="background:url(images/aboutus6.jpg) no-repeat">
+    <h1>about us</h1>
+</div>
+
+<!-- about section starts -->
+
+<section class="about">
+    <div class="image">
+        <img src="images/aboutone.jpg" alt="a photo of a family">
+    </div>
+
+    <div class="content">
+        <h3>why choose us?</h3>
+        <p>Our team comprises knowledgeable guides and wildlife enthusiasts who possess extensive expertise in Sri Lanka's diverse ecosystems and wildlife habitats. We specialize in curating personalized safari itineraries that cater to your interests, whether you seek encounters with majestic elephants, elusive leopards, or colorful birdlife.<br>
+        Moreover, Safari Lanka is dedicated to responsible tourism. We prioritize wildlife conservation and sustainable practices, ensuring that our adventures have a positive impact on local communities and the environment. When you choose Safari Lanka, you not only embark on a thrilling safari but also contribute to the preservation of Sri Lanka's natural heritage.</p>
+        <div class="icons-container">
+            <div class="icons">
+                <i class="fas fa-map"></i>
+                <span>top destinations</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-hand-holding-usd"></i>
+                <span>affordable price</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-headset"></i>
+                <span>24/7 guide service</span>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- about section ends -->
